a2991fd80977ed2c7909f7c9eb4f92009da2f8e3
tech-aziz <aziz.swe71@gmail.com>
2023-12-25 14:08:37 +0600

    created home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      primarySwatch: Colors.blue,
+        useMaterial3: false,
       ),
-      // home: const HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
